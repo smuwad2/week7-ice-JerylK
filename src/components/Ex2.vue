8d04fc8d7a26c3cc16b35b78e739690fcac4a3a2
JerylK <jeryl.khoo.2024@computing.smu.edu.sg>
2025-10-22 01:14:22 +0000

    ex2 done

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,7 +3,8 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                item: ''
             }
             
         }
@@ -14,5 +15,9 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul>
+        <li v-for="item in items">{{ item }}<button @click="$event.target.parentElement.remove()">Delete !</button></li>
+    </ul>
    
+    <input type="text" v-model="item"><button @click="items.push(item); item=''">Add!</button>
 </template>
